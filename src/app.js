eb45eef698548fe808e3e254ee05138eb682d96c
Jaidealistic <jaisubiksha2006@gmail.com>
2026-04-09 23:32:33 +0530

    fixed chandhana's work

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const { errorMiddleware } = require('./middlewares/error.middleware');
 const logger = require('./shared/utils/logger');
+const { startBillingCronJobs } = require('./modules/billing/cron');
 require('dotenv').config();
 
 const app = express();
@@ -75,6 +76,7 @@ const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`);
+    startBillingCronJobs();
   });
 }
 
